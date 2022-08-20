502f3ecf28cf953135ae000a30d73fcad96f20f2
Diego Braga <diegobraga@hertz.com>
2022-08-20 19:08:16 +0100

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Flex } from "@chakra-ui/react"
+import { Header } from "./components/Header"
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Flex
+      flexDir={'column'}
+      justify={'center'}
+      alignItems='center'
+    >
+      <Flex
+        w='100%'
+        maxW='888px'
+      >
+        <Header />
+      </Flex>
+    </Flex>
   )
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,51 @@
+import { Box, Flex, IconButton, Text } from '@chakra-ui/react'
+import Logo from '../../assets/logo.png'
+import { IoMdCart } from "react-icons/io";
+import { MdLocationPin } from "react-icons/md";
+
+export function Header() {
+  return (
+    <Flex
+      height={'104px'}
+      width='100%'
+      alignItems={'center'}
+      justify='space-between'
+    >
+      <Box>
+        <img src={Logo} />
+      </Box>
+      <Flex>
+        <Flex
+          bg='purple.100'
+          fontSize={'0.8rem'}
+          borderRadius={'4px'}
+          mr='2'
+        >
+          <Flex
+            alignItems={'center'}
+            textAlign='center'
+            p='1'
+          >
+            <MdLocationPin fill='#6B46C1' />
+
+            <Text
+              as='span'
+              color={'purple.600'}
+              ml='1'
+            >
+              Limerick, IE
+            </Text>
+          </Flex>
+        </Flex>
+        <IconButton
+          aria-label='Cart'
+          icon={<IoMdCart
+            fill='#C05621'
+          />}
+          size='sm'
+          bg='yellow.100'
+        />
+      </Flex>
+    </Flex>
+  )
+}
