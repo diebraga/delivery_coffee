214df9ad0d07549c32a33cdf044bd032c8d916fe
Diego Braga <diegobraga@hertz.com>
2022-08-23 21:48:51 +0100

    payment form styles

--- src/components/Payment/PaymentForm.tsx
+++ src/components/Payment/PaymentForm.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, FormControl, FormLabel, Heading, Input, Text } from "@chakra-ui/react";
+import { VscLocation } from "react-icons/vsc";
 
 export function PaymentForm() {
   return (
@@ -19,8 +20,79 @@ export function PaymentForm() {
         h='372px'
         borderRadius={'6px'}
       >
+        <Flex
+          flexDir={'column'}
+          p='8'
+        >
+          <Flex>
+            <Text
+              as='span'
+            >
+              <VscLocation fill='#C47F17' size={28} />
+            </Text>
+            <Box>
+              <Text
+                as='h5'
+                color='#403937'
+                ml='2'
+              >
+                Payment
+              </Text>
+              <Text
+                as='p'
+                color='#574F4D'
+                ml='2'
+                fontSize={['0.8rem']}
+              >
+                The payment is made at the delivery point, Choose your payment method.
+              </Text>
+            </Box>
+          </Flex>
 
-
+          <FormControl>
+            <Input
+              maxW='200px'
+              placeholder="Eircode"
+              mt='8'
+              variant={'filled'}
+              focusBorderColor='#4B2995'
+              bg='#E6E5E5'
+            />
+            <Input
+              placeholder="Address"
+              mt='4'
+              variant={'filled'}
+              focusBorderColor='#4B2995'
+              bg='#E6E5E5'
+            />
+            <Flex mt='4'>
+              <Input
+                placeholder="Apt num."
+                maxW='200px'
+                variant={'filled'}
+                focusBorderColor='#4B2995'
+                bg='#E6E5E5'
+              />
+              <Input
+                placeholder="City"
+                ml='4'
+                variant={'filled'}
+                focusBorderColor='#4B2995'
+                bg='#E6E5E5'
+              />
+            </Flex>
+            <Flex justify={'right'}>
+              <Input
+                placeholder="County"
+                maxW='200px'
+                variant={'filled'}
+                focusBorderColor='#4B2995'
+                bg='#E6E5E5'
+                mt='4'
+              />
+            </Flex>
+          </FormControl>
+        </Flex>
       </Flex>
     </Box>
   )
