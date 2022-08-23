4ae46b1f11e1849c38f71b5d79f286a791647482
Diego Braga <diegobraga@hertz.com>
2022-08-23 09:29:23 +0100

    card component

---
+++ src/components/OurCoffes/CoffeeCard.tsx
@@ -0,0 +1,114 @@
+import { Flex, Heading, Image, Tag, HStack, Text } from "@chakra-ui/react";
+import { QuantityForm } from "./QuantityForm";
+
+interface Tags {
+  label: string
+}
+
+interface Props {
+  imageSrc: string
+  tags: Tags[]
+  title: string
+  content: string
+  price: string
+}
+
+export function CoffeeCard({ imageSrc, tags, content, title, price }: Props) {
+  return (
+    <Flex
+      w='16rem'
+      h='19.375rem'
+      bg='#F3F2F2'
+      borderLeftRadius={'6px'}
+      borderRightRadius='36px'
+      borderBottomLeftRadius={'36px'}
+      borderBottomRightRadius='6px'
+      position={'relative'}
+      alignItems='center'
+      justify={'center'}
+      mt='5'
+      flexDir={'column'}
+    >
+      <Flex
+        position='absolute'
+        top='-1'
+        left={'68px'}
+        w={['120px']}
+        h={['120px']}
+        alignItems='center'
+        justify={'center'}
+        flexDir='column'
+      >
+        <Image
+          src={imageSrc}
+        />
+        <HStack
+          spacing={'1'}
+          mt='2'
+        >
+          {tags.map((tag, i) => {
+            return (
+              <Tag
+                size='sm'
+                bg='#F1E9C9'
+                color={'#C47F17'}
+                fontSize='0.7rem'
+                key={i}
+              >
+                {tag.label}
+              </Tag>
+            )
+          })}
+        </HStack>
+      </Flex>
+      <Flex
+        flexDir={'column'}
+        justify='center'
+        w='80%'
+        textAlign={'center'}
+        mt='9'
+      >
+        <Heading
+          as='h3'
+          fontWeight={'700'}
+          fontSize='1.1rem'
+          p='2'
+        >
+          {title}
+        </Heading>
+        <Text
+          fontSize={'0.8rem'}
+          color='#8D8686'
+        >
+          {content}
+        </Text>
+      </Flex>
+      <Flex
+        position={'absolute'}
+        bottom='3'
+        justify={'space-between'}
+        w='80%'
+      >
+        <Flex position={'relative'} pl='3'>
+          <Text
+            as='span'
+            position={'absolute'}
+            bottom='0.5'
+            left='0'
+          >
+            €
+          </Text>
+          <Text
+            as='span'
+            fontWeight={'800'}
+            fontSize='1.3rem'
+          >
+            {price}
+          </Text>
+        </Flex>
+        <Flex>
+          <QuantityForm />
+        </Flex>
+      </Flex>
+    </Flex>)
+}
--- src/components/OurCoffes/index.tsx
+++ src/components/OurCoffes/index.tsx
@@ -1,8 +1,10 @@
-import { Flex, Heading, Wrap, WrapItem, Image, Tag, HStack, Text } from "@chakra-ui/react";
+import { Flex, Heading, Wrap, WrapItem } from "@chakra-ui/react";
 import Cafe from '../../assets/coffee/cold_espresso.png'
-import { QuantityForm } from "./QuantityForm";
+import { useWindowSize } from "../../hooks/useWindowSize";
+import { CoffeeCard } from "./CoffeeCard";
 
 export function OurCoffes() {
+  const { width } = useWindowSize()
   return (
     <Flex
       flexDir={'column'}
@@ -14,127 +16,55 @@ export function OurCoffes() {
       >
         Our Coffee
       </Heading>
-      <Wrap spacing='30px' flex='1' mt='49px'>
+      <Wrap mt='49px' justify={width && width > 821 ? 'space-between' : 'space-around'}>
         <WrapItem>
-          <Flex
-            w='16rem'
-            h='19.375rem'
-            bg='#F3F2F2'
-            borderLeftRadius={'6px'}
-            borderRightRadius='36px'
-            borderBottomLeftRadius={'36px'}
-            borderBottomRightRadius='6px'
-            position={'relative'}
-            alignItems='center'
-            justify={'center'}
-            mt='5'
-            flexDir={'column'}
-          >
-            <Flex
-              position='absolute'
-              top='-1'
-              left={'68px'}
-              w={['120px']}
-              h={['120px']}
-              alignItems='center'
-              justify={'center'}
-              flexDir='column'
-            >
-              <Image
-                src={Cafe}
-              />
-              <HStack
-                spacing={'1'}
-                mt='2'
-              >
-                <Tag
-                  size='sm'
-                  bg='#F1E9C9'
-                  color={'#C47F17'}
-                  fontSize='0.7rem'
-                >
-                  Traditional
-                </Tag>
-                <Tag
-                  size='sm'
-                  bg='#F1E9C9'
-                  color={'#C47F17'}
-                  fontSize='0.7rem'
-                >
-                  Cold
-                </Tag>
-              </HStack>
-            </Flex>
-            <Flex
-              flexDir={'column'}
-              justify='center'
-              w='80%'
-              textAlign={'center'}
-              mt='9'
-            >
-              <Heading
-                as='h3'
-                fontWeight={'700'}
-                fontSize='1.1rem'
-                p='2'
-              >
-                Cold Espresso
-              </Heading>
-              <Text
-                fontSize={'0.8rem'}
-                color='#8D8686'
-              >
-                Delicious coffee espresso drink made with ice.
-              </Text>
-            </Flex>
-            <Flex
-              position={'absolute'}
-              bottom='3'
-              justify={'space-between'}
-              w='80%'
-            >
-              <Flex position={'relative'} pl='3'>
-                <Text
-                  as='span'
-                  position={'absolute'}
-                  bottom='0.5'
-                  left='0'
-                >
-                  €
-                </Text>
-                <Text
-                  as='span'
-                  fontWeight={'800'}
-                  fontSize='1.3rem'
-                >
-                  9.90
-                </Text>
-              </Flex>
-              <Flex>
-                <QuantityForm />
-              </Flex>
-            </Flex>
-          </Flex>
+          <CoffeeCard
+            imageSrc={Cafe}
+            tags={[
+              { label: 'Traditional' },
+              { label: 'Cold' }
+            ]}
+            title={'Cold Espresso'}
+            content={'Delicious coffee espresso drink made with ice.'}
+            price={'9,90'}
+          />
+        </WrapItem>
+
+        <WrapItem>
+          <CoffeeCard
+            imageSrc={Cafe}
+            tags={[
+              { label: 'Traditional' },
+              { label: 'Cold' }
+            ]}
+            title={'Cold Espresso'}
+            content={'Delicious coffee espresso drink made with ice.'}
+            price={'9,90'}
+          />
         </WrapItem>
         <WrapItem>
-          <Flex w='180px' h='80px' bg='green.200'>
-            Box 2
-          </Flex>
+          <CoffeeCard
+            imageSrc={Cafe}
+            tags={[
+              { label: 'Traditional' },
+              { label: 'Cold' }
+            ]}
+            title={'Cold Espresso'}
+            content={'Delicious coffee espresso drink made with ice.'}
+            price={'9,90'}
+          />
         </WrapItem>
         <WrapItem>
-          <Flex w='180px' h='80px' bg='tomato'>
-            Box 3
-          </Flex>
-        </WrapItem>
-        <WrapItem>
-          <Flex w='180px' h='80px' bg='blue.200'>
-            Box 4
-          </Flex>
-        </WrapItem>
-        <WrapItem>
-          <Flex w='180px' h='80px' bg='blackAlpha.500'>
-            Box 5
-          </Flex>
+          <CoffeeCard
+            imageSrc={Cafe}
+            tags={[
+              { label: 'Traditional' },
+              { label: 'Cold' }
+            ]}
+            title={'Cold Espresso'}
+            content={'Delicious coffee espresso drink made with ice.'}
+            price={'9,90'}
+          />
         </WrapItem>
       </Wrap>
     </Flex>
